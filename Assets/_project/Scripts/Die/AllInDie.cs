ec33d5420d7963b9a74bf9fc3992a525f49a58c9
Jari Bou <bourdietome@gmail.com>
2026-09-11 11:31:23 +0200

    [MOD] All In Dice + Dice launcher tweaks

---
+++ Assets/_project/Scripts/Die/AllInDie.cs
@@ -0,0 +1,23 @@
+﻿using _project.Scripts.ScriptableObjects.Dice.Effects;
+using UnityEngine;
+
+namespace _project.Scripts.Die
+{
+    public class AllInDie : DiceBase
+    {
+        [SerializeField] protected AllInDieLingeringEffect effect;
+        
+    #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public override async Awaitable ApplyEffect(GameState gamestate)
+    #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            int upFaceValue = GetUpFaceValue();
+            SpawnScoreEffect(upFaceValue);
+            gamestate.AddScore(upFaceValue);
+            if (upFaceValue == 1)
+            {
+                gamestate.AddEffect(effect);
+            }
+        }
+    }
+}
